b9b7ea01d92a07637ce49f10afe54e531b82b372
Vincent Fischer <vincent.i.fischer@gmail.com>
2025-06-05 11:46:49 +0200

    fix: getaddrinfo ENOTFOUND "base"

--- src/lib/postgres-drizzle/database.ts
+++ src/lib/postgres-drizzle/database.ts
@@ -1,3 +1,8 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 
-export const db = drizzle(process.env.POSTGRESQL_DATABASE_URL!);
+const connectionString = process.env.POSTGRESQL_DATABASE_URL!
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(connectionString, { prepare: false })
+export const db = drizzle(client);
