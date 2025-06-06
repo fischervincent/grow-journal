06071229b02899e8da8223dda4d23d4be60d5c40
Vincent Fischer <vincent.i.fischer@gmail.com>
2025-06-06 12:48:12 +0200

    fix ts build

--- src/core/use-cases/find-plants-by-user.ts
+++ src/core/use-cases/find-plants-by-user.ts
@@ -1,9 +1,9 @@
-import { PlantWithId } from "../domain/plant";
+import { PlantWithPhotoAndId } from "../domain/plant";
 import { PlantRepository } from "../repositories/plant-repository";
 
 
 export const findPlantsByUserUseCase = (plantRepository: PlantRepository) => {
-  const findPlantsByUser = async (userId: string): Promise<PlantWithId[]> => {
+  const findPlantsByUser = async (userId: string): Promise<PlantWithPhotoAndId[]> => {
     const plants = await plantRepository.findByUserId(userId);
     return plants;
   }
